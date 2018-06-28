432893b4aa4126fad9241eeaddeaa9c4ca9a5ffe
WavyPeng <w1a9v9y3@gmail.com>
2018-06-28 15:05:22 +0800

    添加微信用户实体

---
+++ src/main/java/com/wavy/o2o/controller/wechat/WechatLoginController.java
@@ -0,0 +1,16 @@
+package com.wavy.o2o.controller.wechat;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by WavyPeng on 2018/6/28.
+ */
+@Controller
+@RequestMapping("/wechatlogin")
+public class WechatLoginController {
+    private Logger log = LoggerFactory.getLogger(WechatLoginController.class);
+
+}
